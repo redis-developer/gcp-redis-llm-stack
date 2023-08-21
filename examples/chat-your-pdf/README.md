ae3e938eff1c75a1be1ee819ecc46ff586f6783d
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-08-21 16:48:54 -0400

    WIP on demo

--- examples/chat-your-pdf/README.md
+++ examples/chat-your-pdf/README.md
@@ -5,49 +5,46 @@ This example app shows how to build a simple chatbot powered by Redis and Google
 1) a Jupyter notebook that demonstrates setting up Redis as a vector database to store and retrieve document vectors. The notebook also shows how to use LangChain to perform semantic search for context within a pdf.
 2) a Streamlit app that showcases the chat completion functionality of Vertex AI Palm 2 Chat Completion model, powered by Redis for RAG (retrieval augmented generation), Semantic Caching for the LLM, and chat history persistence.
 
-https://user-images.githubusercontent.com/13009163/237002719-26e3118d-77ee-4ded-96f5-6ba801cae66c.mov
+{ IMAGE FORTHCOMING }
 
 
 ## Setting up the Environment
 
-The tutorial can run in multiple ways. The first step is to configure the ``.env`` file in this repository. This file contains the following variables:
+### Obtain Google Cloud Credentials
+You will need a GCP project, service account, an an application JSON key file in order to auth with GCP. The credentials file will be mounted to the docker container of the chatbot app and exposed with the `GOOGLE_APPLICATION_CREDENTIALS` environment variable. For more information [check out this link](https://cloud.google.com/docs/authentication/application-default-credentials#GAC).
+
+
+1) Download a GCP credentials JSON file:
+    - Go to "IAM & Admin" panel in the GCP console.
+    - On the left navbar select "Service Accounts".
+    - Select the name of the service account.
+    - On the top bar, select the "Keys" tab.
+    - Above the list of active keys, select "ADD KEY" to create a new JSON key file.
+2) **Move the credentials file into the root level of this folder here as `gcp_credentials.json`**
+    ```bash
+    mv ~/Downloads/.......json  gcp_credentials.json
+    ```
+
+### Update environment file
+The project comes with a template `.env.template` file with the following values. Make a coy of this as `.env`. Update the values below accordingly.
 
 ```bash
-OPENAI_TEMPERATURE=0.7
-OPENAI_MAX_TOKENS=50
 CHUNK_SIZE=500
 CHUNK_OVERLAP=100
-
-REDIS_ADDRESS=localhost
-REDIS_PORT=6379
-REDIS_PASSWORD=
+DOCS_FOLDER="pdfs/"
+REDIS_URL="redis://localhost:6379"
+PROJECT_ID="YOUR_GCP_PROJECT_NAME" #nifty-456098
+LOCATION="YOUR_VERTEXAI_REGION"    #us-central1
 ```
 
-### Redis Cloud on GCP Marketplace
-Once you set up your Redis Enterprise database on GCP, please provide the following credentials to your `.env` file:
-
-```bash
-REDIS_PASSWORD=<your password here>
-REDIS_HOST=<your redis address here>
-REDIS_PORT=<your redis port here>
-```
-
-#### Manual Setup (GCP Console)
-For each, the following options are required.
-
-#### Terraform Setup
+Update the `PROJECT_ID` and `LOCATION` based on your GCP project and vertex AI configuration. Update the `REDIS_URL` based on your Redis Enterprise database deployed in GCP.
 
 
+## Run
 
-There are some ``docker-compose.yml`` files in the ``docker`` directory that will help spin up
-redis-stack locally and redisinsight in the case where a remote Redis is being used (like ACRE).
-
-### Run
-
-
-To run the script, follow these steps:
+To run the app, follow these steps:
 
 1. Clone this repository to your local machine.
-2. copy the ``.env.template`` to ``.env`` and configure the values as outlined above.
-3. If using a local Python environment, just run the notebook
-3. Otherwise, select a docker-compose file to run and execute ``docker-compose up`` to start the environment.
+2. Set up your GCP credentials as outlined above.
+2. Copy the `.env.template` to `.env` and configure the values as outlined above.
+3. Run the app with Docker compose: `docker-compose up`.
--- examples/chat-your-pdf/main.py
+++ examples/chat-your-pdf/main.py
@@ -1,6 +1,7 @@
 import os
-import tempfile
 import streamlit as st
+import vertexai
+import uuid
 
 from langchain.chat_models import ChatVertexAI
 from langchain.document_loaders import PyPDFLoader
@@ -13,38 +14,41 @@ from langchain.vectorstores import Redis
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.messages import SystemMessage
 
-from redisvl.llmcache import SemanticCache
+# from redisvl.llmcache import SemanticCache
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
+vertexai.init(project=os.environ['PROJECT_ID'], location=os.environ['LOCATION'])
+
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = uuid.uuid4().hex
+
 st.set_page_config(page_title="Chat Your PDF", page_icon="📃")
 st.title("📃 Chat Your PDF")
 
 
-@st.cache_resource(ttl="1h")
-def configure_retriever(uploaded_files):
+@st.cache_resource()
+def configure_retriever(path):
     # Read documents
     docs = []
-    temp_dir = tempfile.TemporaryDirectory()
-    for file in uploaded_files:
-        temp_filepath = os.path.join(temp_dir.name, file.name)
-        with open(temp_filepath, "wb") as f:
-            f.write(file.getvalue())
-        loader = PyPDFLoader(temp_filepath)
+    for file in os.listdir(path):
+        print(file, flush=True)
+        loader = PyPDFLoader(os.path.join(path, file))
         docs.extend(loader.load())
 
     # Split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
     splits = text_splitter.split_documents(docs)
 
     # Create embeddings and store in vectordb
     embeddings = VertexAIEmbeddings()
-    vectordb = Redis.from_documents(splits, embeddings)
+    vectordb = Redis.from_documents(splits, embeddings, redis_url=os.environ["REDIS_URL"])
 
     # Define retriever
-    retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4})
+    retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 
     return retriever
 
@@ -59,24 +63,24 @@ class StreamHandler(BaseCallbackHandler):
         self.container.markdown(self.text)
 
 
-class PrintRetrievalHandler(BaseCallbackHandler):
-    def __init__(self, container):
-        self.container = container.expander("Context Retrieval")
+# class PrintRetrievalHandler(BaseCallbackHandler):
+#     def __init__(self, container):
+#         self.container = container.expander("Context Retrieval")
 
-    def on_retriever_start(self, query: str, **kwargs):
-        self.container.write(f"**Question:** {query}")
+#     def on_retriever_start(self, query: str, **kwargs):
+#         self.container.write(f"**Question:** {query}")
 
-    def on_retriever_end(self, documents, **kwargs):
-        # self.container.write(documents)
-        for idx, doc in enumerate(documents):
-            source = os.path.basename(doc.metadata["source"])
-            self.container.write(f"**Document {idx} from {source}**")
-            self.container.markdown(doc.page_content)
+#     def on_retriever_end(self, documents, **kwargs):
+#         # self.container.write(documents)
+#         for idx, doc in enumerate(documents):
+#             source = os.path.basename(doc.metadata["source"])
+#             self.container.write(f"**Document {idx} from {source}**")
+#             self.container.markdown(doc.page_content)
 
 
 def reset_msg_history(msgs: RedisChatMessageHistory):
     msgs.clear()
-    msgs.add_message(SystemMessage("""You are a friendly AI assistant that can help a user understand their chosen PDF document.
+    msgs.add_message(SystemMessage(content="""You are a friendly AI assistant that can help a user understand their chosen PDF document.
                                    They will ask questions about the document in question and you can respond with known information driven by facts alone.
                                    You may not discuss arbitrary topics or veer too far off course in converation."""))
     msgs.add_ai_message("How can I help you?")
@@ -91,7 +95,10 @@ def render():
     retriever = configure_retriever(os.environ["DOCS_FOLDER"])
 
     # Setup memory for contextual conversation
-    msgs = RedisChatMessageHistory()
+    msgs = RedisChatMessageHistory(
+        session_id=st.session_state.session_id,
+        url=os.environ["REDIS_URL"]
+    )
     memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
 
     # Setup LLM and QA chain
@@ -102,30 +109,31 @@ def render():
         llm, retriever=retriever, memory=memory, verbose=True
     )
 
-    # Setup LLMCache
-    llmcache = SemanticCache(
-        redis_url=os.environ["REDIS_URL"],
-        threshold=0.9, # semantic similarity threshold
-    )
+    # # Setup LLMCache
+    # llmcache = SemanticCache(
+    #     redis_url=os.environ["REDIS_URL"],
+    #     threshold=0.9, # semantic similarity threshold
+    # )
 
     if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
-        reset_msg_history()
+        reset_msg_history(msgs)
 
     avatars = {"human": "user", "ai": "assistant"}
     for msg in msgs.messages:
-        st.chat_message(avatars[msg.type]).write(msg.content)
+        if msg.type in avatars:
+            st.chat_message(avatars[msg.type]).write(msg.content)
 
     if user_query := st.chat_input(placeholder="Ask me about your pdf!"):
         st.chat_message("user").write(user_query)
 
         with st.chat_message("assistant"):
-            retrieval_handler = PrintRetrievalHandler(st.container())
+            # retrieval_handler = PrintRetrievalHandler(st.container())
             stream_handler = StreamHandler(st.empty())
-            if result := llmcache.check(user_query):
-                for token in result[0].split(" "):
-                    stream_handler.on_llm_new_token(token)
-            else:
-                response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+            # if result := llmcache.check(user_query):
+            #     for token in result[0].split(" "):
+            #         stream_handler.on_llm_new_token(token)
+            # else:
+            response = qa_chain.run(user_query, callbacks=[stream_handler])
 
 
 if __name__ == "__main__":
