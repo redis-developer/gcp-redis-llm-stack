5101f213f3423b2dbf783a7010e6de9638317c8c
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-08-23 10:58:12 -0400

    Add config

---
+++ examples/chat-your-pdf/app/config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class AppConfig:
+    DOCS_FOLDER=os.environ["DOCS_FOLDER"]
+    REDIS_URL=os.environ["REDIS_URL"]
+    GCP_PROJECT_ID=os.environ["GCP_PROJECT_ID"]
+    GCP_LOCATION=os.environ["GCP_LOCATION"]
+    GOOGLE_APPLICATION_CREDENTIALS=os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    CHUNK_SIZE=os.getenv("CHUNK_SIZE", 500)
+    CHUNK_OVERLAP=os.getenv("CHUNK_OVERLAP", 10)
+    PAGE_TITLE=os.getenv("PAGE_TITLE", "📃 Chat Your PDF")
+    PAGE_ICON=os.getenv("PAGE_ICON", "📃")
+    RETRIEVE_TOP_K=int(os.getenv("RETRIEVE_TOP_K", 5))
+    LLMCACHE_THRESHOLD=float(os.getenv("LLMCACHE_THRESHOLD", 0.75))
