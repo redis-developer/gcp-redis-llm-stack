50b8929379bd40e96a741a83541c3034d5c89214
gmflau <gmflau@gmail.com>
2023-08-23 16:14:47 -0700

    Cast to int
    Cast CHUNK_SIZE and CHUNK_OVERLAP to type int

--- examples/chat-your-pdf/app/config.py
+++ examples/chat-your-pdf/app/config.py
@@ -7,8 +7,8 @@ class AppConfig:
     GCP_PROJECT_ID=os.environ["GCP_PROJECT_ID"]
     GCP_LOCATION=os.environ["GCP_LOCATION"]
     GOOGLE_APPLICATION_CREDENTIALS=os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-    CHUNK_SIZE=os.getenv("CHUNK_SIZE", 500)
-    CHUNK_OVERLAP=os.getenv("CHUNK_OVERLAP", 10)
+    CHUNK_SIZE=int(os.getenv("CHUNK_SIZE", 500))
+    CHUNK_OVERLAP=int(os.getenv("CHUNK_OVERLAP", 10))
     PAGE_TITLE=os.getenv("PAGE_TITLE", "📃 Chat Your PDF")
     PAGE_ICON=os.getenv("PAGE_ICON", "📃")
     RETRIEVE_TOP_K=int(os.getenv("RETRIEVE_TOP_K", 5))
